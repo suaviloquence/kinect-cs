e616961c3a99516f786cf994cd322ae73e343b2b
BRHS-PLTW-20 <551730@my.cuhsd.org>
2023-03-10 16:38:01 -0800

    fix frame detection and test floor plane vector

--- KinectCs/Program.cs
+++ KinectCs/Program.cs
@@ -23,18 +23,27 @@ namespace KinectCs
 
             reader.FrameArrived += p.Reader_FrameArrived;
 
+            /* while (true)
+            {
+                var frame = reader.AcquireLatestFrame();
+
+        //        p.handleFrame(frame);
+
+                System.Threading.Thread.Sleep(100);
+            } */
             System.Threading.Thread.Sleep(-1);
         }
 
-        private  void Reader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+    private  void Reader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+       // private void handleFrame(BodyFrame frame)
         {
 
-            var frame = e.FrameReference.AcquireFrame();
-            System.Console.WriteLine("Got frame");
+             var frame = e.FrameReference.AcquireFrame();
+            // System.Console.WriteLine("Got frame");
 
             if (frame == null) return;
          
-            System.Console.WriteLine(frame.BodyCount);
+            // System.Console.WriteLine(frame.BodyCount);
 
             if (frame.BodyCount == 0) return;
 
@@ -42,16 +51,31 @@ namespace KinectCs
 
             frame.GetAndRefreshBodyData(bodies);
 
-            var body = bodies[0];
+            foreach (var body in bodies)
+            {
+                if (!body.IsTracked)
+                {
+                    // System.Console.WriteLine("Skipping untracked body.");
+                    continue;
+                }
 
-            var left = body.Joints[JointType.HandTipLeft];
-            var right = body.Joints[JointType.HandTipRight];
+                // System.Console.WriteLine($"Is body tracked? {body.IsTracked}");
+                // System.Console.WriteLine($"Body # {body.TrackingId}");
+                var left = body.Joints[JointType.HandTipLeft];
+                var right = body.Joints[JointType.HandTipRight];
 
-            var hand = (left == null) ? right : left;
-            if (hand == null) return;
+                // System.Console.WriteLine($"- Left Hand state: {left.TrackingState}");
 
-            System.Console.WriteLine($"{left.Position.X} {left.Position.Y}");
-            System.Console.WriteLine($"{right.Position.X} {right.Position.Y}");
+                var hand = (left == null) ? right : left;
+                // System.Console.WriteLine("- No hands found, skipping...");
+                if (hand == null) continue;
+
+                System.Console.WriteLine($"- Clip: {frame.FloorClipPlane.X} {frame.FloorClipPlane.Y}, {frame.FloorClipPlane.Z}");
+
+                System.Console.WriteLine($"- Left Hand Tip: ({left.Position.X}, {left.Position.Y}, {left.Position.Z})");
+                // System.Console.WriteLine($" - Right Hand Tip: ({right.Position.X}, {right.Position.Y})");
+            }
+            frame.Dispose();
         }
     }
 }
